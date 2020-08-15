d37d4e8ff9f5d68e06ff33141e6c11b02689d71e
Arthur Correnson <arthur.correnson@gmail.com>
2020-08-15 18:09:00 +0200

    Add cnf conversion with proofs

--- src/language/logic.v
+++ src/language/logic.v
@@ -1,6 +1,5 @@
 Require Import ZArith Psatz.
 Require Import Lists.List.
-Require Import Lists.ListSet.
 Require Import Recdef.
 Require Import Program.Wf.
 Require Import Program.Equality.
@@ -33,7 +32,9 @@ Inductive logic_2 : Set :=
   | L2or    : logic_2 -> logic_2 -> logic_2
   | L2lit   : literal -> logic_2.
 
-Inductive logic_3 : Set :=.
+Definition clause := list literal.
+
+Definition logic_3 := list clause.
 
 (* Conversion algorithm from logic to logic_1 *)
 Fixpoint remove_impl (l : logic) : logic_1 :=
@@ -66,6 +67,26 @@ Fixpoint remove_neg' (sign : bool) (l : logic_1) : logic_2 :=
   end.
 
 Definition remove_neg := remove_neg' true.
+
+Definition merge_clause (c : clause) (l : logic_3) :=
+  map (fun c' => c ++ c') l.
+
+Fixpoint merge (l1 : logic_3) (l2 : logic_3) : logic_3 :=
+  match l1 with
+  | [] => []
+  | c::cs => (merge_clause c l2) ++ (merge cs l2)
+  end.
+
+Fixpoint cnf' (l : logic_2) : logic_3 :=
+  match l with
+  | L2and a b => (cnf' a) ++ (cnf' b)
+  | L2or a b => merge (cnf' a) (cnf' b)
+  | L2lit a => [[a]]
+  end.
+
+Definition cnf (l : logic) : logic_3 :=
+    cnf' (remove_neg (remove_impl l)).
+
 
 Fixpoint semL (l : logic) (v : nat -> bool) : bool :=
   match l with
@@ -101,6 +122,18 @@ Fixpoint semL2 (l : logic_2) (v : nat -> bool) : bool :=
   | L2lit (Neg n) => negb (v n)
   end.
 
+Fixpoint semC (c : clause) (v : nat -> bool) : bool :=
+  match c with
+  | [] => false
+  | Pos n::xs => orb (v n) (semC xs v)
+  | Neg n::xs => orb (negb (v n)) (semC xs v)
+  end.
+
+Fixpoint semL3 (l : logic_3) (v : nat -> bool) : bool :=
+  match l with
+  | [] => true
+  | c::cs => andb (semC c v) (semL3 cs v)
+  end.
 
 Theorem remove_impl_correct:
   forall l v, semL l v = semL1 (remove_impl l) v.
@@ -135,4 +168,75 @@ Proof.
   - rewrite IHl. unfold remove_neg. simpl.
     rewrite remove_neg_false_negb; reflexivity.
   - reflexivity.
+Qed.
+
+
+Lemma merge_comm:
+  forall l v, semL3 (merge l []) v = semL3 (merge [] l) v.
+Proof.
+  intros.
+  induction l; simpl.
+  - reflexivity.
+  - rewrite IHl. simpl; reflexivity.
+Qed.
+
+Theorem semC_union:
+  forall c1 c2 v, semC (c1 ++ c2) v = orb (semC c1 v) (semC c2 v).
+Proof.
+  intros.
+  induction c1; simpl.
+  - reflexivity.
+  - destruct a; rewrite IHc1, Bool.orb_assoc; reflexivity.
+Qed.
+
+Theorem semL3_union:
+  forall c1 c2 v, semL3 (c1 ++ c2) v = andb (semL3 c1 v) (semL3 c2 v).
+Proof.
+  intros.
+  induction c1; simpl.
+  - reflexivity.
+  - rewrite IHc1, Bool.andb_assoc; reflexivity.
+Qed.
+
+Theorem merge_clause_correct:
+  forall c l v, semL3 (merge_clause c l) v = orb (semC c v) (semL3 l v).
+Proof.
+  intros.
+  induction l; simpl.
+  - rewrite Bool.orb_comm. reflexivity.
+  - rewrite semC_union, IHl, Bool.orb_andb_distrib_r; reflexivity.
+Qed.
+
+Theorem merge_correct:
+  forall l1 l2 v, orb (semL3 l1 v) (semL3 l2 v) = semL3 (merge l1 l2) v.
+Proof.
+  intros.
+  induction l1; simpl.
+  - reflexivity.
+  - rewrite semL3_union,
+            merge_clause_correct,
+            <- IHl1,
+            Bool.orb_andb_distrib_l.
+    reflexivity.
+Qed.
+
+Theorem cnf'_correct:
+  forall l v, semL2 l v = semL3 (cnf' l) v.
+Proof.
+  intros.
+  induction l; simpl.
+  - rewrite semL3_union, IHl1, IHl2. reflexivity.
+  - rewrite <- merge_correct, IHl1, IHl2. reflexivity.
+  - destruct l; rewrite Bool.andb_comm, Bool.orb_comm; simpl; reflexivity.
+Qed.
+
+Theorem cnf_correct:
+  forall l v, semL l v = semL3 (cnf l) v.
+Proof.
+  intros.
+  unfold cnf.
+  rewrite remove_impl_correct,
+          remove_neg_correct,
+          cnf'_correct.
+  reflexivity.
 Qed.
